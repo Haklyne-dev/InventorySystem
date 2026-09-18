c69fdd2ec9702f015a511632fc12844c8bfcfca2
MooreCoder27 <137838319+Jzmoore27@users.noreply.github.com>
2026-09-18 11:51:56 -0400

    undo progress indicators in install script

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -16,15 +16,10 @@ $LatestTag = git -C "$AppDir\repo" tag --sort=-v:refname | Select-Object -First 
 git -C "$AppDir\repo" checkout $LatestTag
 
 python -m venv "$AppDir\venv"
-Write-Progress -Activity "Installing Python Packages" -Status "Running pip install..." -PercentComplete -1
-& "$AppDir\venv\Scripts\python.exe" -m pip install -r "$AppDir\repo\requirements.txt" *>$null
-Write-Progress -Activity "Installing Python Packages" -Completed
-
-Write-Progress -Activity "Upgrading pip" -Status "Running pip install --upgrade pip..." -PercentComplete -1
-& "$AppDir\venv\Scripts\python.exe" -m pip install --upgrade pip *>$null
-Write-Progress -Activity "Upgrading pip" -Completed
+& "$AppDir\venv\Scripts\python.exe" -m pip install -r "$AppDir\repo\requirements.txt"
+& "$AppDir\venv\Scripts\python.exe" -m pip install --upgrade pip
 
 Copy-Item "$AppDir\repo\scripts\launcher.cmd" "$BinDir\InventorySystem.cmd" -Force
 
 Write-Host "Installed InventorySystem $LatestTag"
-Write-Host "Start the server by running 'InventorySystem' from the command line."
+Write-Host "Start the server by running 'InventorySystem.cmd' from the command line."
